55538616e4daffed94e04ea497d40fe4f8b6d77e
Konstantsin <k4164724@gmail.com>
2025-08-17 21:34:07 +0300

    4

--- main.js
+++ main.js
@@ -1,54 +1,14 @@
-// положение корабля
-// const random = Math.floor(Math.random()*5)
-// const loc1 = random ;
-// const loc2 = loc1 + 1;
-// const loc3 = loc2 + 1;
-
-// let isSunk = false; 
-// let guess;
-// let hits = 0 ;
-// // создаем счетчик выстрелов
-// let guesses = 0;
-
-
-// while (isSunk === false) {
-//     guess = +prompt("Fire!Enter a number 0-6");
-//     if (guess  < 0 || guess > 6) {
-//         alert(" Please Enter valid number!")
-//     } else {
-// // увеличиваем счетчик выстрелов
-// guesses += 1;
+function makePhrases () {
     
-//          if (guess === loc1||guess === loc2||guess === loc3)  {
-//               alert("hit")
-//                  hits = hits + 1;
-//              if (hits === 3) {
-//                isSunk = true;
-//                  alert("die");
-//         }
-//              } else {
-//         alert("miss")
-//         }
-//     }
-// }
-// const stat = "You have " + hits + " hit out of " + guesses + " shots " 
-// alert(stat);
-const depositAmount = 1000;
-const yearlyRate = 15;
-const depositTermInYears = 2 ;
 
-    function getDepositIncome (amount , rate ,term){
-        let depositIncome = 0;
-        let count = 0;
-    while (count < term){
-        depositIncome = depositIncome + amount * (rate / 100);
-        count ++ ;
-    }
-    return depositIncome ;
-    }
+const words1 = ["Мой кот", " Моя собака" , "Мой попугай"];
+const words2 = ["любит есть" , "хочет погрызть" , "всегда ищет"];
+const words3 = ["морковку" , "макароны" , "косточку"];
 
-     const depositIncome_1 = getDepositIncome(depositAmount , yearlyRate , depositTermInYears) ;   
-     const depositIncome_2 = getDepositIncome(1500 , 16 , 3);
-     const total = depositIncome_1 + depositIncome_2;
-     console.log(total);
-     
+const randomIndex_1 = Math.floor(Math.random()*words1.length)
+const randomIndex_2 = Math.floor(Math.random()*words2.length)
+const randomIndex_3 = Math.floor(Math.random()*words3.length)
+const phrase = words1[randomIndex_1] + " "+ words2[randomIndex_2] + " " + words3[randomIndex_3]
+return phrase ;
+}
+alert(makePhrases());
