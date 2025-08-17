a7189fffcfc50886b2c3a93ce51db05713226e14
Konstantsin <k4164724@gmail.com>
2025-08-17 21:42:10 +0300

    4

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 function getRandomElement(array) {
     const randomIndex = Math.floor(Math.random()*array.length)
-    return array.[randomIndex]
+    return array[randomIndex];
 }
 
 function makePhrases () {
@@ -11,10 +11,6 @@ const words1 = ["Мой кот", " Моя собака" , "Мой попугай
 const words2 = ["любит есть" , "хочет погрызть" , "всегда ищет"];
 const words3 = ["морковку" , "макароны" , "косточку"];
 
-const randomIndex_1 = Math.floor(Math.random()*words1.length)
-const randomIndex_2 = Math.floor(Math.random()*words2.length)
-const randomIndex_3 = Math.floor(Math.random()*words3.length)
-const phrase = words1[randomIndex_1] + " "+ words2[randomIndex_2] + " " + words3[randomIndex_3]
-return phrase ;
+return getRandomElement(words1) + " "+ getRandomElement(words2) + " " + getRandomElement(words3)
 }
 alert(makePhrases());
